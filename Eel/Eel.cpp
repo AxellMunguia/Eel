9ae1263cc8c83650e7f2e72796f86d812754abac
Axell Munguia <ajmung01@louisville.edu>
2024-11-20 19:25:31 -0500

    Added namespace.

--- Eel/Eel.cpp
+++ Eel/Eel.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World! This is a fresh start :) \n";
+    cout << "Hello World! This is a fresh start :) \n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
